845a6c640915ae0f80782e0156c2ec6ad50584ed
Martin Bonnin <martin@mbonnin.net>
2022-07-17 19:35:33 +0200

    initial commit

---
+++ Casks/pin-github-actions.rb
@@ -0,0 +1,15 @@
+cask "pin-github-actions" do
+    arch = Hardware::CPU.intel? ? "x64" : "arm64"
+  
+    version "0.1.0"
+  
+    sha256 :no_check
+  
+    url "https://github.com/martinbonnin/pin-github-actions/releases/download/v#{version}/pin-github-actions-#{arch}.zip"
+    name "pin-github-actions"
+    desc "Pin your GitHub actions to their commit sha"
+    homepage "https://github.com/martinbonnin/pin-github-actions"
+  
+    binary "pin-github-actions/pin-github-actions"
+  
+  end
